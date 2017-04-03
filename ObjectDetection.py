63ad7e8fe6fca1f8f9086a6ba337d4ae7a396b6d
chcp <chcp@cin.ufpe.br>
2017-04-03 10:51:40 -0300

    update main, adding vector of all imgs

--- ObjectDetection.py
+++ ObjectDetection.py
@@ -5,16 +5,18 @@ import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import sys
-sys.path.insert(0, '/Example')
+sys.path.insert(0, 'Example')
 import cart2PolarOpencv as HOG
 
-src = cv2.imread("Data/src/1.jpg")
-src = cv2.cvtColor(src, cv2.COLOR_BGR2RGB)
-src = cv2.pyrDown(src)
-src = cv2.pyrDown(src)
-src = cv2.pyrDown(src)
-src = cv2.pyrDown(src)
-plt.imshow(src)
+VAR = "Data/positive/"
+TYPEFILE = ".jpg"
+lista = []
+for i in range(1,13233):#13233
+    src = cv2.imread(VAR+str(i)+TYPEFILE)
+    src = cv2.pyrDown(src)
+    src = cv2.pyrDown(src)
+    histG = HOG.getHistogramOfGradients(src)
+    lista.append(histG)
+    if i%133 == 0:
+        print str(int( i/13233.0 * 100))+"%"
 
-histG = HOG.getHistogramOfGradients(src)
-print histG
