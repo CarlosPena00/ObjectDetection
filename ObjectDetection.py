6585e989819f0066ea5e574bb90181566e05045a
chcp <chcp@cin.ufpe.br>
2017-04-03 13:05:51 -0300

    Adding CSV file

--- ObjectDetection.py
+++ ObjectDetection.py
@@ -7,16 +7,22 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.insert(0, 'Example')
 import cart2PolarOpencv as HOG
-
+NUM_OF_POSITIVE_IMG = 13233
 VAR = "Data/positive/"
 TYPEFILE = ".jpg"
 lista = []
-for i in range(1,13233):#13233
+for i in range(1,NUM_OF_POSITIVE_IMG+1):#13233
     src = cv2.imread(VAR+str(i)+TYPEFILE)
     src = cv2.pyrDown(src)
     src = cv2.pyrDown(src)
     histG = HOG.getHistogramOfGradients(src)
+    
     lista.append(histG)
+    #lista.extend(histG)
+    
     if i%133 == 0:
-        print str(int( i/13233.0 * 100))+"%"
+        print str(int( i/float(NUM_OF_POSITIVE_IMG) * 100))+"%"
+
+X = np.array(lista)
+np.savetxt("CSV/Positive_Samples.csv",X,delimiter= ",",fmt="%.2f")
 
