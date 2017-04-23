c80de0c50c26ab96d7aeebb718477714886802b8
chcp <chcp@cin.ufpe.br>
2017-04-23 14:14:50 -0300

    Adding comments due long time to calculate

--- Example/svm.py
+++ Example/svm.py
@@ -24,19 +24,21 @@ def svm (X,y,Kernel = 'linear'):
     print (cm[0,0]+cm[1,1])/float(cm.sum())
     return X_train,X_test,y_train,y_test,y_pred,classifier,cm,sc_X
 
-def randomF(X,y, N = 100):
-    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.30 , random_state = 0)
-    sc_X = StandardScaler()
-    X_train = sc_X.fit_transform(X_train)
-    X_test = sc_X.transform(X_test)
-    classifier = RandomForestClassifier(n_estimators=N,n_jobs=4,criterion='entropy')
-    classifier.fit(X_train,y_train)
-    y_pred = classifier.predict(X_test)
-    cm = confusion_matrix(y_test,y_pred)
-    print cm
-    print (cm[0,0]+cm[1,1])/float(cm.sum())
-    
-    return X_train,X_test,y_train,y_test,y_pred,classifier,cm,sc_X
+def randomF(X,y, N = 100,theads = 3):
+	print "----- Start To Split -----"
+	X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.30,  random_state = 0)
+	sc_X = StandardScaler()
+	print "----- Start Classifier -----"
+	X_train = sc_X.fit_transform(X_train)
+	X_test = sc_X.transform(X_test)
+	classifier = RandomForestClassifier(n_estimators=N,n_jobs=theads,criterion='entropy')
+	classifier.fit(X_train,y_train)
+	y_pred = classifier.predict(X_test)
+	cm = confusion_matrix(y_test,y_pred)
+	print cm
+	print (cm[0,0]+cm[1,1])/float(cm.sum())
+
+	return X_train,X_test,y_train,y_test,y_pred,classifier,cm,sc_X
 
 
 """   
