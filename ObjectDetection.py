00cf39793f5b0ee8df3e4b2c84726276c1298e9d
chcp <chcp@cin.ufpe.br>
2017-04-29 16:38:41 -0300

    Correct box

--- ObjectDetection.py
+++ ObjectDetection.py
@@ -196,6 +196,7 @@ def train(classifier, stdScaler, std=0):
             xMax = bx[2]
             yMax = bx[3]
             cv2.rectangle(src2, (yMin, xMin), (yMax, xMax), (0, 255, 0))
+            #print "Box detectado"
         cv2.imwrite("ID" + str(ID) + "Rect.jpg", src2)
         print "The ID: " + str(ID)
 
