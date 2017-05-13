7d82f0cad65f8f93901926d57f3f052932de1485
Heitor Rapela <hrm@cin.ufpe.br>
2017-05-13 15:17:34 -0300

    Add F2LBP file and change lbpFeatures to the same model of HOG

---
+++ F2LBP.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 21 12:33:02 2017
+
+@author: Carlos Pena
+@author: Heitor Rapela
+"""
+
+import cv2
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import sys
+sys.path.insert(0, 'Source')
+import HOG as HOG
+sys.path.insert(0, 'LBP')
+import lbpFeatures as LBP
+import time
+IMSIZE = 76
+
+def folds2LBP(fromFile=0, positive=1, argMin=0, argMax=100, Blur = 0):
+    DATAFOLDER = "Data/"
+    CSVFOLDER = "CSV/"
+    if positive == 0 and fromFile == 0:
+        CSVFOLDER += "Negative/"
+        data = pd.read_csv("Data/negativeList.csv")
+    if positive == 1 and fromFile == 0:
+        CSVFOLDER += "Positive/"
+        data = pd.read_csv("Data/positiveList.csv")
+
+    foldName = data.iloc[:, :].values
+    minIndex = max(argMin, 0)
+    maxIndex = min(foldName.shape[0], argMax)
+    for i in range(minIndex, maxIndex):
+        FOLDER = foldName[i][0]
+        TYPEFILE = "." + foldName[i][1]
+        CUT = foldName[i][2]
+        NUMBER_OF_IMG = foldName[i][3]
+        SAVEFILE = FOLDER[:-1] + ".csv"
+        print "----- Start Folder: " + FOLDER + " -----"
+        time.sleep(1)
+        fold2LBP(DATAFOLDER, CSVFOLDER, FOLDER,
+                 TYPEFILE, CUT, NUMBER_OF_IMG, SAVEFILE, Blur)
+
+
+def fold2LBP(DATAFOLDER, CSVFOLDER, FOLDER, TYPEFILE, CUT, NUMBER_OF_IMG, SAVEFILE, Blur):
+    lista = np.empty([1,1764]) #np.empty([1, 2369])
+    for f in tqdm(range(1, NUMBER_OF_IMG + 1)):
+        src = cv2.imread(DATAFOLDER + FOLDER + str(f) + TYPEFILE)
+        rows, cols, channel = src.shape
+            
+        if rows > 1 and cols > 1:
+        
+            if Blur == 1:
+                src = cv2.pyrUp(cv2.pyrDown(src))
+            rows, cols, channel = src.shape
+            
+            if CUT == 1:
+                maxRows = rows / IMSIZE
+                maxCols = cols / IMSIZE
+            if CUT == 0:
+                maxRows = 1
+                maxCols = 1
+            for j in range(0, maxRows):
+                for i in range(0, maxCols):
+                    if CUT == 1:
+                        roi, xMin, xMax, yMin, yMax = HOG.getROIsrc(
+                            src, j, i, px=IMSIZE)
+                    if CUT == 0:
+                        roi = src
+                    rowsR, colsR, channel = roi.shape
+                    
+                    if rowsR < 1 or colsR < 1:
+                        print "F2H Fold2LBP erro Size"
+                        continue
+                    
+                    if rowsR != IMSIZE or colsR != IMSIZE:
+                        roi = cv2.resize(roi, (IMSIZE, IMSIZE))
+                    histLBP = LBP.getHistogramOfLBP(roi)
+                    
+                    lista = np.vstack((lista, histLBP))
+        else:
+            print "Error Img not fond"
+            histLBP = np.zeros(2369)
+            lista = np.vstack((lista, histLBP))
+
+    X = np.delete(lista, (0), axis=0)
+    np.savetxt(CSVFOLDER + SAVEFILE, X, delimiter=",", fmt="%f")
+
+
+if __name__ == "__main__":
+    print "Positive min max Blur"
+    if len(sys.argv) > 4:
+        pos = int(sys.argv[1])
+        minV = int(sys.argv[2])
+        maxV = int(sys.argv[3])
+        Blur = int(sys.argv[4])
+        
+        print minV, maxV
+    else:
+        print "Argv no info, using default"
+        pos = 0
+        minV = 5
+        maxV = 6
+        Blur = 0
+    folds2LBP(positive=pos, argMin=minV, argMax=maxV + 1, Blur=Blur)
--- LBP/lbpFeatures.py
+++ LBP/lbpFeatures.py
@@ -5,49 +5,50 @@ import math
 import matplotlib.pyplot as plt
 import time
 
-# Open image
-image = cv2.imread("teste.jpg")
-# Convert image bgr to gray
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# get rows and cols len of the image
-rows = image.shape[0]
-cols = image.shape[1]
-
-#cv2.imshow("image",image)
-#cv2.waitKey(0)
-
-# initialize empty list of lbp features
-myList = []
-for i in range(1,rows-1):
-	for j in range(1,cols-1):
-		# Boards are not treated
-		#if(~(i == rows-1 or i == 1 or j == cols-1 or j == 0)):
-			# Get central pixel to calculate lbp features
-		pc = image[i,j]
-
-		p0 = 1 if image[i-1,j-1] >= pc else 0
-		p1 = 1 if image[i-1,j] >= pc else 0
-		p2 = 1 if image[i-1,j+1] >= pc else 0
-		p3 = 1 if image[i,j+1] >= pc else 0
-		p4 = 1 if image[i+1,j+1] >= pc else 0
-		p5 = 1 if image[i+1,j] >= pc else 0
-		p6 = 1 if image[i+1,j-1] >= pc else 0
-		p7 = 1 if image[i,j-1] >= pc else 0
-
-		p0 = (int(p0))	
-		p1 = (int(p1)* (2 << 0))
-		p2 = (int(p2)* (2 << 1))
-		p3 = (int(p3)* (2 << 2))
-		p4 = (int(p4)* (2 << 3))
-		p5 = (int(p5)* (2 << 4))
-		p6 = (int(p6)* (2 << 5))
-		p7 = (int(p7)* (2 << 6))
-		summ = p0+p1+p2+p3+p4+p5+p6+p7
-		summ = int(summ)
-		myList.append(summ)
-
 # Plot Lbp data
-plt.hist(myList, bins=np.arange(0,255), align='left', normed='True')
-plt.xlim(0,255)
-plt.show()
+def plotLBPHist(myList):
+	plt.hist(myList, bins=np.arange(0,255), align='left', normed='True')
+	plt.xlim(0,255)
+	plt.show()
+
+# Get Histogram of LBP an return a np array of it
+def getHistogramOfLBP(image):
+	# Convert to Grayscale
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+	# get rows and cols len of the image
+	rows = image.shape[0]
+	cols = image.shape[1]
+
+	# initialize empty list of lbp features
+	fullLBP = np.float32()
+	for i in range(1,rows-1):
+		for j in range(1,cols-1):
+			cellLBP = np.float32()
+			pc = image[i,j]
+
+			p0 = 1 if image[i-1,j-1] >= pc else 0
+			p1 = 1 if image[i-1,j] >= pc else 0
+			p2 = 1 if image[i-1,j+1] >= pc else 0
+			p3 = 1 if image[i,j+1] >= pc else 0
+			p4 = 1 if image[i+1,j+1] >= pc else 0
+			p5 = 1 if image[i+1,j] >= pc else 0
+			p6 = 1 if image[i+1,j-1] >= pc else 0
+			p7 = 1 if image[i,j-1] >= pc else 0
+
+			p0 = (int(p0))	
+			p1 = (int(p1)* (2 << 0))
+			p2 = (int(p2)* (2 << 1))
+			p3 = (int(p3)* (2 << 2))
+			p4 = (int(p4)* (2 << 3))
+			p5 = (int(p5)* (2 << 4))
+			p6 = (int(p6)* (2 << 5))
+			p7 = (int(p7)* (2 << 6))
+			summ = p0+p1+p2+p3+p4+p5+p6+p7
+			summ = int(summ)
+			cellLBP = np.float32(summ)
+			fullLBP = np.append(fullLBP, cellLBP)
+    fullLBPMatrix = np.asmatrix(fullLBP)
+    return fullLBPMatrix
+
+
